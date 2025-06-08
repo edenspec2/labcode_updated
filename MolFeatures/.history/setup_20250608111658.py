4d219d445d24a269887bf436e5a34c71e62ea63c
edenspec2 <edenspec@post.bgu.ac.il>
2025-06-08 11:21:00 +0300

    update

--- MolFeatures/.history/setup_20250608111658.py
+++ MolFeatures/.history/setup_20250608111658.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='MolFeatures',
-    version='0.9006',
+    version='0.9005',
     packages=find_packages(),
     description='Your package description',
     long_description=long_description,
